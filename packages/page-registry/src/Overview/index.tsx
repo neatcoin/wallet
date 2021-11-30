9eee153abeabb29f7c3e0d790337e5ec4d523951
Wei Tang <wei@that.world>
2021-11-30 17:49:52 +0000

    Implement basic registry overview page

--- packages/page-registry/src/Overview/index.tsx
+++ packages/page-registry/src/Overview/index.tsx
@@ -2,13 +2,71 @@
 // SPDX-License-Identifier: GPL-3.0
 
 import type { ComponentProps as Props } from '../types';
+import { useApi, useCall } from '@polkadot/react-hooks';
+import { useTranslation } from '../translate';
+import { Table } from '@polkadot/react-components';
+import { AddressSmall, SummaryBox, CardSummary, Spinner } from '@polkadot/react-components';
 
-import React from 'react';
+import React, { useMemo } from 'react';
+
+function hexToName(hex: string): string {
+  var ret = ""
+  for (var c = 2; c < hex.length; c += 2) {
+    ret += String.fromCharCode(parseInt(hex.substr(c, 2), 16));
+  }
+  return ret;
+}
+
+function nameToString(name: string[]): string {
+  var ret = "";
+  for (var i = 0; i < name.length; i++) {
+    if (ret === "") {
+      ret = hexToName(name[i]);
+    } else {
+      ret = hexToName(name[i]) + "." + ret;
+    }
+  }
+  if (ret === "") {
+    return "[root]"
+  } else {
+    return ret
+  }
+}
 
 function Overview ({ className = '' }: Props): React.ReactElement<Props> {
+  const { t } = useTranslation();
+  const { api } = useApi();
+
+  const ownerships = useCall<unknown>(api.query.registry.ownerships.entries);
+
   return (
     <div className={className}>
-      <p>Hello, world!</p>
+      <SummaryBox>
+        <section>
+          <CardSummary label='Domain count'>
+            {ownerships ? ownerships.length : <Spinner noLabel />}
+          </CardSummary>
+        </section>
+      </SummaryBox>
+      <Table
+        empty={!ownerships}
+        header={[['Registered domains', 'start', 2], ['current owner', 'expand']]}
+      >
+        {(ownerships || []).map(([namehash, valueRaw]): React.ReactNode => {
+          const value = JSON.parse(valueRaw.toString());
+          const name = value[0];
+          const ownership = value[1];
+          if (ownership.account) {
+            return (
+              <tr key={namehash.toString()}>
+                <td>{nameToString(name)}</td>
+                <td className='expand all'></td>
+                <td className='address'><AddressSmall value={ownership.account} /></td>
+              </tr>
+            );
+          }
+        })}
+      </Table>
     </div>
   );
 }

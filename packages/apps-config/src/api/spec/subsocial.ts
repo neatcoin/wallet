2bffb7cbf44e555af6cf33d239f64fed4c55a3fb
Wei Tang <wei@that.world>
2021-11-30 14:58:37 +0000

    Remove all other networks to reduce maintanence burden

--- packages/apps-config/src/api/spec/subsocial.ts
+++
@@ -1,6 +0,0 @@
-// Copyright 2017-2021 @polkadot/apps-config authors & contributors
-// SPDX-License-Identifier: Apache-2.0
-
-import { types } from '@subsocial/types/substrate/preparedTypes';
-
-export default { types };
--- packages/apps-config/src/endpoints/index.ts
+++ packages/apps-config/src/endpoints/index.ts
@@ -6,9 +6,7 @@ import type { LinkOption } from './types';
 
 import { createCustom, createDev, createOwn } from './development';
 import { createProduction } from './production';
-import { createKusamaRelay, createPolkadotRelay } from './productionRelays';
 import { createTesting } from './testing';
-import { createRococoRelay, createWestendRelay } from './testingRelays';
 
 export { CUSTOM_ENDPOINT_KEY } from './development';
 
@@ -19,52 +17,11 @@ export function createWsEndpoints (t: TFunction, firstOnly = false, withSort = t
       isDisabled: false,
       isHeader: true,
       isSpaced: true,
-      text: t('rpc.header.polkadot.relay', 'Polkadot & parachains', { ns: 'apps-config' }),
-      textBy: '',
-      value: ''
-    },
-    ...createPolkadotRelay(t, firstOnly, withSort),
-    {
-      isDisabled: false,
-      isHeader: true,
-      text: t('rpc.header.kusama.relay', 'Kusama & parachains', { ns: 'apps-config' }),
-      textBy: '',
-      value: ''
-    },
-    ...createKusamaRelay(t, firstOnly, withSort),
-    {
-      isDisabled: false,
-      isHeader: true,
-      isSpaced: true,
-      text: t('rpc.header.westend.relay', 'Test Westend & parachains', { ns: 'apps-config' }),
-      textBy: '',
-      value: ''
-    },
-    ...createWestendRelay(t, firstOnly, withSort),
-    {
-      isDisabled: false,
-      isHeader: true,
-      text: t('rpc.header.rococo.relay', 'Test Rococo & parachains', { ns: 'apps-config' }),
-      textBy: '',
-      value: ''
-    },
-    ...createRococoRelay(t, firstOnly, withSort),
-    {
-      isDisabled: false,
-      isHeader: true,
-      isSpaced: true,
-      text: t('rpc.header.live', 'Live networks', { ns: 'apps-config' }),
+      text: t('rpc.header.live', 'Neatcoin networks', { ns: 'apps-config' }),
       textBy: '',
       value: ''
     },
     ...createProduction(t, firstOnly, withSort),
-    {
-      isDisabled: false,
-      isHeader: true,
-      text: t('rpc.header.test', 'Test networks', { ns: 'apps-config' }),
-      textBy: '',
-      value: ''
-    },
     ...createTesting(t, firstOnly, withSort),
     {
       isDevelopment: true,

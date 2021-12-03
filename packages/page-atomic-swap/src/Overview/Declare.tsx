cd15144d7196dfdaf359ba601e5db38ebe5fc4d1
Wei Tang <wei@that.world>
2021-12-03 16:45:05 +0000

    Function to declare swaps as initiator and counterparty

---
+++ packages/page-atomic-swap/src/Overview/Declare.tsx
@@ -0,0 +1,110 @@
+// Copyright 2021 @neatcoin/page-atomic-swap authors & contributors
+// SPDX-License-Identifier: GPL-3.0
+
+import React, { useState } from 'react';
+
+import { Button, Input, InputAddress, InputBalance, InputNumber, Modal, TxButton } from '@polkadot/react-components';
+import { useApi, useModal } from '@polkadot/react-hooks';
+import { blake2AsHex } from '@polkadot/util-crypto';
+
+import { useTranslation } from '../translate';
+
+interface Props {
+  className?: string;
+  type: 'initiator' | 'counterparty';
+}
+
+function Declare ({ className = '', type }: Props): React.ReactElement<Props> | null {
+  const { t } = useTranslation();
+  const { api } = useApi();
+  const { isOpen, onClose, onOpen } = useModal();
+
+  const [secretOrHash, setSecretOrHash] = useState<string>('');
+  const [source, setSource] = useState<string | null>(null);
+  const [target, setTarget] = useState<string | null>(null);
+  const [value, setValue] = useState(null);
+  const [duration, setDuration] = useState(null);
+
+  return (
+    <>
+      {isOpen && (
+        <Modal
+          header={t<string>('Declare as ' + type)}
+          onClose={onClose}
+        >
+          <Modal.Content>
+            <InputAddress
+              help={t<string>('Select the sender account.')}
+              label={t<string>('sender account')}
+              onChange={setSource}
+              type='account'
+              withLabel
+            />
+            <InputAddress
+              help={t<string>('Select the receiver account.')}
+              label={t<string>('receiver account')}
+              onChange={setTarget}
+              type='account'
+              withLabel
+            />
+            {type === 'initiator' ? <>
+              <Input
+                className='medium'
+                help={t<string>('Secret for the atomic swap.')}
+                label={t<string>('secret')}
+                onChange={setSecretOrHash}
+                value={secretOrHash}
+              />
+              <Input
+                className='disabledLook'
+                help={t<string>('Secret hash to give to the counterparty.')}
+                label={t<string>('secret hash')}
+                value={blake2AsHex(secretOrHash)}
+              />
+            </> : <>
+              <Input
+                className='medium'
+                help={t<string>('Secret hash from the initiator.')}
+                label={t<string>('secret hash')}
+                onChange={setSecretOrHash}
+                value={secretOrHash}
+              />
+            </>}
+            <InputBalance
+              help={t<string>('Value to send to the other party.')}
+              label={t<string>('value')}
+              value={value}
+              onChange={setValue}
+            />
+            <InputNumber
+              help={t<string>('Duration of the swap.')}
+              label={t<string>('duration')}
+              value={duration}
+              onChange={setDuration}
+            />
+          </Modal.Content>
+          <Modal.Actions>
+            {type === 'initiator' ? <TxButton
+              accountId={source}
+              onStart={onClose}
+              params={[target, blake2AsHex(secretOrHash), { value }, duration]}
+              tx={api.tx.atomicSwap.createSwap}
+            /> : <TxButton
+              accountId={source}
+              onStart={onClose}
+              params={[target, secretOrHash, { value }, duration]}
+              tx={api.tx.atomicSwap.createSwap}
+            />}
+          </Modal.Actions>
+        </Modal>
+      )}
+      <Button
+        icon='plus'
+        label={t<string>('Declare as ' + type)}
+        onClick={onOpen}
+      />
+    </>
+  );
+}
+
+export default React.memo(Declare);
--- packages/page-atomic-swap/src/Overview/index.tsx
+++ packages/page-atomic-swap/src/Overview/index.tsx
@@ -4,13 +4,14 @@
 import { PendingSwap, PendingSwapEntries } from '@neatcoin/types/interfaces';
 import React from 'react';
 
-import { AddressSmall, Table } from '@polkadot/react-components';
+import { AddressSmall, Table, SummaryBox, Button, CardSummary, Spinner } from '@polkadot/react-components';
 import { useApi, useEventTrigger, useMapEntries } from '@polkadot/react-hooks';
 import { FormatBalance } from '@polkadot/react-query';
 import { Bytes, Option } from '@polkadot/types';
 import { ITuple } from '@polkadot/types/types';
 
 import { useTranslation } from '../translate';
+import Declare from './Declare';
 
 interface Props {
   className?: string;
@@ -29,12 +30,24 @@ function Overview ({ className = '' }: Props): React.ReactElement<Props> {
 
   return (
     <div className={className}>
+      <SummaryBox>
+        <section>
+          <CardSummary label={t<string>('Pending count')}>
+            {pendingSwaps ? pendingSwaps.length : <Spinner noLabel />}
+          </CardSummary>
+        </section>
+        <Button.Group>
+          <Declare type='initiator' />
+          <Declare type='counterparty' />
+        </Button.Group>
+      </SummaryBox>
       <Table
         empty={!pendingSwaps}
-        header={[[t('Pending swaps'), 'start', 2], [t('initiator'), 'expand'], [t('end block')], [t('value')]]}
+        header={[[t('Pending swaps'), 'start', 2], [t('sender'), 'expand'], [(t('receiver'))], [t('end block')], [t('value')]]}
       >
         {(pendingSwaps || []).map((entry: ITuple<[Bytes, Option<PendingSwap>]>): React.ReactNode | null => {
           const secretHash = '0x' + Buffer.from(entry[0].slice(-32)).toString('hex');
+          const receiver = '0x' + Buffer.from(entry[0].slice(-80, -48)).toString('hex');
 
           if (entry[1].isSome) {
             const value = entry[1].unwrap();
@@ -42,12 +55,11 @@ function Overview ({ className = '' }: Props): React.ReactElement<Props> {
             const action = value.action;
             const endBlock = value.endBlock;
 
-            console.log(action);
-
             return (<tr key={source.toString() + ':' + secretHash.toString()}>
               <td>{secretHash}</td>
               <td className='expand all'></td>
               <td className='address'><AddressSmall value={source} /></td>
+              <td className='address'><AddressSmall value={receiver} /></td>
               <td>{endBlock.toString()}</td>
               <td><FormatBalance value={action.value} /></td>
             </tr>);

08c81aaa8e6ebaa1977e6c4dbcc6889a78aafc22
Wei Tang <wei@that.world>
2021-12-04 15:23:35 +0000

    Remove unused spec code

--- packages/apps-config/src/api/chain/index.ts
+++
@@ -1,9 +0,0 @@
-// Copyright 2021 @neatcoin/apps-config authors & contributors
-// SPDX-License-Identifier: GPL-3.0
-
-import { typesChain } from '@phala/typedefs';
-
-// alphabetical, based on the actual displayed name
-export default {
-  ...typesChain
-};
